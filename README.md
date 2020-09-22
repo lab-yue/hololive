90ae9a7ae20e437e023902f8714fa6b815aa2080
rainy-me <github@rainy.me>
2020-09-23 06:23:04 +0900

    feat: fetch titles

--- README.md
+++ README.md
@@ -9,5 +9,13 @@ get hololive streaming schedule from [schedule.hololive.tv](https://schedule.hol
 hololive
 
 # show all streams recently
+hololive -a
 hololive --all
+
+# show streams with titles
+hololive -t
+hololive --title
+
+# show all streams with titles
+hololive -a -t
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,66 @@
 use clap::{App, Arg};
 use colored::*;
+use futures::StreamExt;
 use pad::PadStr;
-use regex::{Captures,Match, Regex};
+use regex::{Captures, Match, Regex};
+use std::fmt;
+use tokio;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Stream {
     member: String,
     url: String,
     start: String,
     is_streaming: bool,
+    title: String,
+}
+impl fmt::Display for Stream {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{:<6} {} {:<11} {} {}",
+            self.start.magenta(),
+            self.member.bold().pad_to_width_with_char(15, ' '),
+            if self.is_streaming {
+                " streaming ".black().on_bright_green().to_string()
+            } else {
+                "".to_string()
+            },
+            self.url.replace("https://www.youtube.com/watch?v=",""),
+            self.title.replace(" - YouTube","").yellow(),
+        )
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("hololive")
         .version("v0.0.1")
-        .arg(Arg::with_name("all").long("all").help("show all"))
+        .arg(Arg::with_name("a").short("a").long("all").help("show all"))
+        .arg(
+            Arg::with_name("t")
+                .short("t")
+                .long("title")
+                .help("show titles"),
+        )
         .get_matches();
 
-    let res = reqwest::get("https://schedule.hololive.tv/")
+    let text = reqwest::get("https://schedule.hololive.tv/")
         .await?
         .text()
-        .await
-        .unwrap();
-    let mut schedule = get_schedule(&res);
-    if matches.occurrences_of("all") == 0 {
+        .await?;
+    let mut schedule = get_schedule(&text, matches.occurrences_of("t") != 0).await;
+    if matches.occurrences_of("a") == 0 {
         schedule.retain(|s| s.is_streaming);
     }
-
-    for stream in schedule {
-        println!(
-            "{:<6} {} {:<9} {}",
-            stream.start.magenta(),
-            stream.member.bold().pad_to_width_with_char(15, ' '),
-            if stream.is_streaming { "streaming".bright_green().to_string() } else { "".to_string() },
-            stream.url,
-        );
+    for x in schedule {
+        println!("{}", x);
     }
+
     Ok(())
 }
 
-fn get_schedule(base: &str) -> Vec<Stream> {
+async fn get_schedule(text: &str, with_title: bool) -> Vec<Stream> {
     let re = Regex::new(
         r#"(?x)
     thumbnail"[\s\S]+?
@@ -50,14 +70,30 @@ fn get_schedule(base: &str) -> Vec<Stream> {
     "#,
     )
     .unwrap();
-    re.captures_iter(base)
-        .map(|cap| Stream {
-            member: match_or_empty(cap.name("member") ),
-            url: match_or_empty(cap.name("url") ),
-            start: match_or_empty(cap.name("start")),
-            is_streaming: match_or_empty(cap.get(0)).contains("red solid"),
+    let streams = futures::stream::iter(re.captures_iter(text).map(get_match))
+        .map(|s| {
+            let mut stream = s;
+            async move {
+                if with_title {
+                    stream.title = get_url_title(&stream.url).await.unwrap();
+                };
+                stream
+            }
         })
+        .buffer_unordered(10)
         .collect::<Vec<Stream>>()
+        .await;
+    streams
+}
+
+fn get_match<'a>(cap: Captures<'a>) -> Stream {
+    Stream {
+        member: match_or_empty(cap.name("member")),
+        url: match_or_empty(cap.name("url")),
+        start: match_or_empty(cap.name("start")),
+        is_streaming: match_or_empty(cap.get(0)).contains("red solid"),
+        title: "".to_string(),
+    }
 }
 
 fn match_or_empty(maybe_match: Option<Match>) -> String {
@@ -67,14 +103,36 @@ fn match_or_empty(maybe_match: Option<Match>) -> String {
     }
 }
 
+async fn get_url_title(url: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let text = reqwest::get(url).await?.text().await?;
+    Ok(get_title(&text))
+}
+
+fn get_title(text: &str) -> String {
+    let re = Regex::new(r#"<title>([\s\S]+?)</title>"#).unwrap();
+    match re.captures(text) {
+        Some(matched) => match_or_empty(matched.get(1)),
+        _ => "".to_string(),
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn test_get_schedule() {
-        let test_html = include_str!("./test.html");
-        let mut schedule = crate::get_schedule(test_html);
+    const TEST_HTML: &str = include_str!("./test.html");
+    #[tokio::test]
+    async fn test_get_schedule() -> Result<(), Box<dyn std::error::Error>> {
+        let mut schedule = crate::get_schedule(TEST_HTML, false).await;
         assert_eq!(schedule.len(), 69);
         schedule.retain(|s| s.is_streaming);
         assert_eq!(schedule.len(), 6);
+        Ok(())
+    }
+    #[test]
+    fn test_get_title() {
+        let title = crate::get_title(TEST_HTML);
+        assert_eq!(
+            title,
+            "ホロライブプロダクション配信予定スケジュール『ホロジュール』".to_string()
+        );
     }
 }
